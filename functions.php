8ce11355719ba6bef2b248029db5e9d8c9a80fe6
Ricardo Rosendo <31318399+ricardorosendo0@users.noreply.github.com>
2021-02-28 01:56:29 -0500

    Update functions.php
    Nuevas funciones, quitar intervalo de precios y botón agregar carro

--- functions.php
+++ functions.php
@@ -28,3 +28,29 @@ function my_loginlogo() {
   add_filter('login_headerurl', 'my_loginURL');
 
 // END ENQUEUE PARENT ACTION
+
+ // Quitar intervalo de precios en productos variables de WooCommerce
+function quitar_intervalo( $price, $product ) {
+    // Precio normal
+    $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+    $price = $prices[0] !== $prices[1] ? sprintf( __( 'Desde: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+ 
+    // Precio rebajado
+    $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+    sort( $prices );
+    $saleprice = $prices[0] !== $prices[1] ? sprintf( __( 'Desde: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+ 
+    if ( $price !== $saleprice ) {
+        $price = '<del>' . $saleprice . '</del> <ins>' . $price . '</ins>';
+    }
+     
+    return $price;
+}
+ 
+add_filter( 'woocommerce_variable_sale_price_html', 'quitar_intervalo', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'quitar_intervalo', 10, 2 );
+//Fin de quitar precios
+
+
+/* Botón de añadir al carrito en tienda */
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 20 );
