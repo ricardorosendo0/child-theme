a52e83d0d501f0d1f2a8139355f4cd05c02ee9f9
Ricardo Rosendo <31318399+ricardorosendo0@users.noreply.github.com>
2022-05-05 13:10:18 -0500

    Función eliminar campos de dirección
    elimine los campos de facturación si el carrito contiene productos virtuales @ WooCommerce Checkout

--- functions.php
+++ functions.php
@@ -168,3 +168,36 @@ function security_wp_login_redirect() {
 }
 add_filter( 'site_url', 'security_wp_login_filter', 10, 3 );
 add_action( 'login_init', 'security_wp_login_redirect' );
+
+
+
+/*Fragmento de PHP: elimine los campos de facturación si el carrito contiene productos virtuales @ WooCommerce Checkout*/
+/**
+ * @snippet       Simplify Checkout if Only Virtual Products
+ * @how-to        Get CustomizeWoo.com FREE
+ * @sourcecode    https://businessbloomer.com/?p=78351
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 3.5.4
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+ 
+add_filter( 'woocommerce_checkout_fields' , 'bbloomer_simplify_checkout_virtual' );
+function bbloomer_simplify_checkout_virtual( $fields ) {
+   $only_virtual = true;
+   foreach( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+      // Check if there are non-virtual products
+      if ( ! $cart_item['data']->is_virtual() ) $only_virtual = false;   
+   }
+    if( $only_virtual ) {
+       unset($fields['billing']['billing_company']);
+       unset($fields['billing']['billing_address_1']);
+       unset($fields['billing']['billing_address_2']);
+       unset($fields['billing']['billing_city']);
+       unset($fields['billing']['billing_postcode']);
+       unset($fields['billing']['billing_country']);
+       unset($fields['billing']['billing_state']);
+       unset($fields['billing']['billing_phone']);
+       add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+     }
+     return $fields;
+}
