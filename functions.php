9361a63a49dd898a82e8387bd66f455c0976e227
Ricardo Rosendo <31318399+ricardorosendo0@users.noreply.github.com>
2022-07-05 09:37:55 -0500

    Update functions.php
    Quitar campos de la página de pago de WooCommerce de manera manual

--- functions.php
+++ functions.php
@@ -201,3 +201,29 @@ function bbloomer_simplify_checkout_virtual( $fields ) {
      }
      return $fields;
 }
+
+/*Quitar campos de la página de pago de WooCommerce de manera manual, 
+Para ir activando los campos que quieres que estén funcionando pon delante: //
+*/
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+
+function custom_override_checkout_fields( $fields ) {
+//    unset($fields['billing']['billing_first_name']); 
+//    unset($fields['billing']['billing_last_name']);
+//    unset($fields['billing']['billing_company']);
+//    unset($fields['billing']['billing_address_1']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_city']);
+//    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_country']);
+//    unset($fields['billing']['billing_state']);
+//    unset($fields['billing']['billing_phone']);
+//    unset($fields['order']['order_comments']);
+//    unset($fields['billing']['billing_address_2']);
+//    unset($fields['billing']['billing_postcode']);
+//    unset($fields['billing']['billing_company']);
+//    unset($fields['billing']['billing_last_name']);
+//    unset($fields['billing']['billing_email']);
+    unset($fields['billing']['billing_city']);
+    return $fields;
+}
