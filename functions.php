ef167b25f500260272923483400abb8a51bee550
Ricardo Rosendo <31318399+ricardorosendo0@users.noreply.github.com>
2023-04-13 10:31:28 -0500

    Cambio de posición del botón de woocommerce
    se anexa función cambio de posición del botón

--- functions.php
+++ functions.php
@@ -234,3 +234,19 @@ function custom_override_checkout_fields( $fields ) {
     echo '<style type="text/css">
     .thwcfd-review-wrapper {display: none;}
         </style>';});
+
+
+/** Mueva el botón Agregar al carrito arriba de la descripción - WooCommerce **/
+/** woocommerce_single_product_summary hook
+*@hooked woocommerce_template_single_title - 5
+*@hooked woocommerce_template_single_price-10
+*@hooked woocommerce_template_single_excerpt - 20
+*@hooked
+woocommerce_template_single_add_to_cart-30
+*@hooked
+woocommerce_template_single_meta - 40
+*@hooked woocommerce_template_single_sharing - 50
+**/
+
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 15);
